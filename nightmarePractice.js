372d8b7b1184b721972c7af48714c7c9d5e4e361
Lissa Walzer <lissawalzer@gmail.com>
2016-01-11 10:41:09 -0800

    Nightmare can pull out multiple elements and loop through pages

--- nightmarePractice.js
+++ nightmarePractice.js
@@ -8,8 +8,52 @@ vo(run)(function(err, result) {
   if (err) throw err;
 });
 
-
 function *run() {
+    var selectorOne = 'p';
+    var selectorTwo = 'h1'
+    var textArr = [];
+    var url = 'http://www.search-seo-engine-optimization.com/';
+
+    var urlExtensions = {
+        0: '',
+        1: 'seattle/pay-per-click-advertising-ppc.html'
+    }
+
+    Nightmare.action('elements', function(done) {
+        this.evaluate_now(function() {
+            var p = document.getElementsByTagName('p');
+            var h1 = document.getElementsByTagName('h1');
+            strP = []
+            strh1 = [];
+            for (idx in p) {
+                strP.push(p[idx].innerText);
+            }
+            for (idx in h1) {
+                strh1.push(h1[idx].innerText);
+            }
+            return {
+                pElements: strP,
+                h1Elements: strh1
+            }
+        }, done)
+    })
+
+    var nightmare = Nightmare();
+        for (var i = 0; i < 2; i++) {
+            var p = yield nightmare
+                .goto(url + urlExtensions[i])
+                .elements()
+                textArr.push(p);
+        }
+
+        yield nightmare.end();
+
+    console.log(textArr);
+}
+
+
+//Working one for wta
+/*function *run() {
     var textArr = [];
 
     var nightmareText = Nightmare();
@@ -35,3 +79,4 @@ function *run() {
         console.log(textArr);
         yield nightmareText.end();
 }
+*/
