01fee63ac5fc8f574de55f5fddb11738947cabe8
HalahRaadSalih <hala.raad.salih@gmail.com>
2016-01-12 14:16:22 -0800

    added home header for user page

--- controllers/users.js
+++ controllers/users.js
@@ -2,21 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 // check if user is logged in with either oAuth or auth
-router.use(function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated() || res.locals.currentUser) { 
-  	return next(); 
-  }
-  else{
+// router.use(function ensureAuthenticated(req, res, next) {
+//   if (req.isAuthenticated() || res.locals.currentUser) { 
+//   	return next(); 
+//   }
+//   else{
 
-  	  res.redirect('/auth/signin');
-  }
-});
+//   	  res.redirect('/auth/signin');
+//   }
+// });
 
 router.get('/users/:id', function(req, res){
 	/*
 		find user in data base and send user to the user page
 	*/
-	res.render('sucess, user is logged in');
+  res.render('users/user');
+	// res.render('sucess, user is logged in');
 });
 
 module.exports = router;
