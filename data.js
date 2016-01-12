b8892d15f9d8c0db7e01adf10a6e04640691d5bf
Lissa Walzer <lissawalzer@gmail.com>
2016-01-12 11:37:38 -0800

    Parses the hikingdata.json to insert into db

--- data.js
+++ data.js
@@ -4,42 +4,46 @@ var fs = require('fs');
 fs.readFile('./hikesData.json', 'utf8', function(err, data) {
     var hikeData = JSON.parse(data);
     var insert = {};
-    var numbers = /\d+(\.\d{1,2})?/;
 
-    for (var i = 0; i < 1; i++) {
+    //Regular expressions to test for hike features
+    var numbers = /\d+(\.\d{1,2})?/;
+    var trailLength = /roundtrip/;
+    var trailGain = /Gain/;
+    var trailHighpoint = /Highest/;
+
+    for (var i = 0; i < hikeData.length; i++) {
         for (var j = 0; j < hikeData[i]['titleElements'].length; j++) {
             insert.title = hikeData[i]['titleElements'][j];
             insert.url = hikeData[i]['urlElements'][j];
             insert.desciption = hikeData[i]['descriptionsElements'][j];
 
             var features = hikeData[i]['featuresElements'][j].split('\n');
+            //Filtering out empty values for features
+            var filteredFeatures = features.filter(function(str) {
+                return /\S/.test(str);
+            });
 
-            var length = numbers.exec(features[0]);
-            if (length) {
-                insert.length = parseFloat(length[0]);
-            } else {
+            //If there are no features, set each value to null
+            if (filteredFeatures.length === 0) {
                 insert.length = null;
-            }
-
-            var elevation = numbers.exec(features[1]);
-            if (elevation) {
-                insert.elevation = parseFloat(elevation[0]);
-            } else {
                 insert.elevation = null;
-            }
-
-            var highPoint = numbers.exec(features[2]);
-            if (highPoint) {
-                insert.highPoint = parseFloat(highPoint[0]);
-            } else {
                 insert.highPoint = null;
+            } else {
+                //Else check each feature against the regular expression to get the appropriate number
+                for (var k = 0; k < filteredFeatures.length; k++) {
+                    if (trailLength.test(filteredFeatures[k])) {
+                        var lengthArr = numbers.exec(filteredFeatures[k]);
+                        insert.length = parseFloat(lengthArr[0]);
+                    } else if (trailGain.test(filteredFeatures[k])) {
+                        var elevationArr = numbers.exec(filteredFeatures[k]);
+                        insert.elevation = parseFloat(elevationArr[0]);
+                    } else if (trailHighpoint.test(filteredFeatures[k])) {
+                        var highPointArr = numbers.exec(filteredFeatures[k]);
+                        insert.highPoint = parseFloat(highPointArr[0]);
+                    }
+                }
             }
-
             console.log(insert);
         }
     }
-
-    //console.log(hikeData[103]['titleElements'].length);
-
-
 })
