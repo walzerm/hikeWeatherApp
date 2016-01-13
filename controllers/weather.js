dbaadf0650e9aea7833f520be5cc95b4f76aa589
HalahRaadSalih <hala.raad.salih@gmail.com>
2016-01-13 13:49:08 -0800

    linked to weather api

--- controllers/weather.js
+++ controllers/weather.js
@@ -3,7 +3,16 @@ var router = express.Router();
 var request = require('request');
 
 router.get('/', function(req, res){
-	request('api.openweathermap.org/data/2.5/weather?lat=35&lon=139', function (error, response, body){
-		console.log(response.body);
+	request('http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139', function (error, response, body){
+		console.log('*************');
+		if(!error){
+			res.send(body);
+		}
+		else{
+			console.log(error);
+			res.send(error);
+		}
 	});
 });
+
+module.exports = router;
