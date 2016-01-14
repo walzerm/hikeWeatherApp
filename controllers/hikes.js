a4eefb909411f9d4d58f1adf4bea448ddd427bea
HalahRaadSalih <hala.raad.salih@gmail.com>
2016-01-13 18:43:04 -0800

    displayed weather and temp on hikes page

--- controllers/hikes.js
+++ controllers/hikes.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var validator  = require('../src/validator');
+var request = require('request');
 
 router.get('/', function(req, res, next){
 	res.render('search/search', {currentWeather:""});
@@ -8,9 +9,29 @@ router.get('/', function(req, res, next){
 
 router.post('/', function(req, res, next){
 	if(validator.zipCode(req.body.zipcode)){
-		console.log(req.body);
 		res.cookie('zipcode', req.body.zipcode);
-		res.redirect('/weather');
+		if(req.cookies.zipcode){
+
+			var apiURL = 'http://api.openweathermap.org/data/2.5/weather'
+			var WEATHER_API_KEY = process.env['WEATHER_API_KEY'];
+			var zipcode = req.cookies.zipcode;
+
+			var url = apiURL+'?zip='+zipcode+',us&APPID='+WEATHER_API_KEY;
+
+			request(url, function (error, response, body){
+				var weather = JSON.parse(body);
+				console.log(weather);
+				if(!error){
+					res.render('search/search', {
+						currentWeather: weather
+					});
+				}
+				else{
+					console.log(error);
+					res.send(error);
+				}
+			});
+		}
 	}
 	else{
 		res.send('not a valid zipCode');
--- controllers/weather.js
+++ controllers/weather.js
@@ -3,23 +3,7 @@ var router = express.Router();
 var request = require('request');
 
 router.get('/', function(req, res){
-	if(req.cookies.zipcode){
-		var url = 'http://api.openweathermap.org/data/2.5/weather?zip='+req.cookies.zipcode+',us&APPID='+process.env['WEATHER_API_KEY'];
-
-		request(url, function (error, response, body){
-			var weather = body;
-
-			if(!error){
-				res.render('search/search', {
-					currentWeather: weather
-				});
-			}
-			else{
-				console.log(error);
-				res.send(error);
-			}
-		});
-	}
+	
 	
 });
 
