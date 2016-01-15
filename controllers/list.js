0b8eb799fed0463bbcfb389174864cb7ce5ed9ad
Lissa Walzer <lissawalzer@gmail.com>
2016-01-14 18:42:11 -0800

    Fixed fb user adding a list

--- controllers/list.js
+++ controllers/list.js
@@ -6,40 +6,47 @@ var knex = require('../db/knex');
 
 
 router.post('/new', function(req, res) {
-
-    var id;
+var userid;
 //Finish so that FB users also get lists, need to get user id from the facebook to add to lists table
     if (!res.locals.currentUser) {
         knex('users').where('facebook_id', req.user.id).first().then(function(userPrimary) {
-            knex('fav_hikes_lists').where('user_id', userPrimary.id).then(function(lists) {
-                res.render('users/user',{
-                    user : req.user,
-                    photo: req.user.photos[0].value,
-                    hikes: lists
-                    });
-            });
+            console.log('I am indeed a fb user');
+            console.log(userPrimary.id);
+            userid = userPrimary.id;
+            knex('fav_hikes_lists').where({
+                list_name: req.body.list,
+                user_id: userPrimary.id
+            }).first().then(function(list) {
+                console.log('I got to insert');
+                if (!list) {
+                    knex('fav_hikes_lists').insert({
+                        list_name: req.body.list,
+                        user_id: userid
+                    }).then(function() {
+                        res.redirect('/users');
+                    })
+                } else {
+                    res.redirect('/users');
+                }
+            })
         })
     } else {
-        knex('users').where('facebook_id', )
-    }
-
-    console.log(req.user.id);
-    knex('fav_hikes_lists').where({
-        list_name: req.body.list,
-        user_id: req.signedCookies.userID
-    }).first().then(function(list) {
-        if(!list) {
-            knex('fav_hikes_lists').insert({
-                list_name: req.body.list,
-                user_id: req.signedCookies.userID
-            }).then(function() {
+        knex('fav_hikes_lists').where({
+            list_name: req.body.list,
+            user_id: req.signedCookies.userID
+        }).first().then(function(list) {
+            if(!list) {
+                knex('fav_hikes_lists').insert({
+                    list_name: req.body.list,
+                    user_id: req.signedCookies.userID
+                }).then(function() {
+                    res.redirect('/users');
+                });
+            } else {
                 res.redirect('/users');
-            });
-        } else {
-            res.redirect('/users');
-        }
-    })
-
+            }
+        })
+    }
 })
 
 router.delete('/delete', function(req, res) {
